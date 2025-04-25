7f245adbb0deeb4f708cd742b250b6f1fd4585dc
Shang-Ru Yang <rime97410000@gmail.com>
2025-04-25 17:48:25 +0800

    [update to class 5] app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask
+from flask import request
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="public",
+    static_url_path="/"
+)
 
 @app.route("/")
 def index():
-    return "Hello, flask"
+    print("請求方法：", request.method)
+    print("通訊協定：", request.scheme)
+    print("主機名稱：", request.host)
+    print("路徑：", request.path)
+    print("完整的網址：", request.url)
+    print("瀏覽器和作業系統：", request.headers.get("user-agent"))
+    print("語言偏好：", request.headers.get("accept-language"))
+    print("引薦網址：", request.headers.get("referrer"))
+    lang = request.headers.get("accept-language")
+    if lang.startswith("en"):
+        return "Hello, flask"
+    else:
+        return "你好，Flask"
+    
 
 @app.route("/data")
 def handleData():
