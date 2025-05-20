2de1bfe1ccecd09af9cc2ac573db26e1b7921a46
Shang-Ru Yang <rime97410000@gmail.com>
2025-05-20 16:47:18 +0800

    [update to class 15] mongo-practice.py

--- mongo-practice.py
+++ mongo-practice.py
@@ -12,12 +12,12 @@ try:
 except Exception as e:
     print(e)
 
-db = client.test # 選擇操作 test 的資料庫 (不存在的話會創建一個)
-collection = db.users # 選擇操作 users 集合
+db = client.website # 選擇操作 test 的資料庫 (不存在的話會創建一個)
+collection = db.members # 選擇操作 users 集合
 
 # 把資料新增到集合中
 collection.insert_one({
-    "name": "汐汐",
-    "gender": "女",
+    "email": "abc@abc.com",
+    "password": "abc",
 })
 print("資料新增成功")
